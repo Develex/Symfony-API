48a0638025379bac76649ed726e6dd575939eec6
FCDevelex <collin.franckena001@fclive.nl>
2019-03-20 08:24:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+api
+===
+
+A Symfony project created on March 15, 2019, 9:00 am.
---
+++ src/AppBundle/Controller/APIController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class APIController extends FOSRestController
+{
+
+    /**
+     * @Rest\Get("/api/get/data")
+     */
+    public function getDataAction() {
+
+        return new JsonResponse(['api' => 'dummy data']);
+
+    }
+
+}
